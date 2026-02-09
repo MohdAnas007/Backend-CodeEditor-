b01afd2475a44c8d7c7eec9714a39967631de011
Mohd Anas <mohdanas08022006@gmail.com>
2026-02-09 10:43:12 +0530

    cors fixed

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(cors(
     {
         origin:[
             "http://localhost:5173",
-            'http://localhost:5174/',
+            'http://localhost:5174',
+            'https://front-end-code-editor-eight.vercel.app'
         ]
         ,
         methods:["GET","POST","PUT","DELETE"],
